45861629e77f856681b33edfc06e4acf7b8f8ea1
Andreas Modahl <a_modahl@hotmail.com>
2018-11-28 17:10:35 +0100

    Update AndroidWebViewManager.java

--- android/src/main/java/com/oblongmana/webviewfileuploadandroid/AndroidWebViewManager.java
+++ android/src/main/java/com/oblongmana/webviewfileuploadandroid/AndroidWebViewManager.java
@@ -38,9 +38,9 @@ public class AndroidWebViewManager extends ReactWebViewManager {
 
     @Override
     protected WebView createViewInstance(ThemedReactContext reactContext) {
-        if (ReactBuildConfig.DEBUG) {
+        //if (ReactBuildConfig.DEBUG) {
             WebView.setWebContentsDebuggingEnabled(true);
-        }
+        //}
         
         WebView view = super.createViewInstance(reactContext);
         //Now do our own setWebChromeClient, patching in file chooser support
